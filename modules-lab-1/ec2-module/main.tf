9b57bed2f5067e908c1301669d8832e25333e61a
Leonardo Viana <leonardoviana00@hotmail.com>
2021-10-28 13:34:25 -0300

    modules-lab-1

---
+++ modules-lab-1/ec2-module/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "module_instance_1" {
+    ami = data.aws_ami.windows.id
+    instance_type = "t2.micro"
+}
+
+data "aws_ami" "windows" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["801119661308"] 
+
+}
